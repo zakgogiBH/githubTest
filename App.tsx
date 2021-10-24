ef520cefa9262ac987ad4173f72acfc6874e4204
zakgogi <zakgogi1@gmail.com>
2021-10-24 21:57:33 +0100

    controller play

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, SafeAreaView, Button } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 
@@ -10,38 +10,86 @@ interface FormInput {
   password: string;
 }
 
-export default function App() {
-
-  const [open, setOpen] = useState(false);
-  const [currentValue, setCurrentValue] = useState(null);
-  const [items, setItems] = useState([{}]);
-  const { register, setValue, handleSubmit } = useForm<FormInput>();
-
-  const RegisterForm = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Email</Text>
-        <TextInput {...register('email')} />
-        <Text>Password</Text>
-        <TextInput {...register('password', { required: true})} />
-        <DropDownPicker 
-          items={items}
-          open={open}
-          value={currentValue}
-          setOpen={setOpen}
-          setValue={setCurrentValue}
-          setItems={setItems}
-        />
-      </View>
-    )
-  }
+export default function App(){
+  const { control, register, handleSubmit, formState: { errors} } = useForm();
+  const onSubmit = (data: FormInput) => console.log(data);
 
   return (
     <View style={styles.container}>
-      <RegisterForm />
+      <Text>Input your email</Text>
+      <Controller 
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: { onChange, value }}) => (
+          <TextInput
+            style={styles.textInput} 
+            {...register('email')}
+            onChangeText={onChange}
+            value={value} 
+          />
+        )}
+        name="email"
+        defaultValue="" 
+      />
+      {errors.email && <Text>This is required.</Text>}
+      <Text>Input your password</Text>
+      <Controller 
+        control={control}
+        rules={{
+          required: true,
+        }}
+        render={({field: { onChange, value }}) => (
+          <TextInput
+            style={styles.textInput}  
+            {...register('password')}
+            onChangeText={onChange}
+            value={value} 
+          />
+        )}
+        name="password"
+        defaultValue="" 
+      />
+      {errors.password && <Text>This is required.</Text>}
+      <Button title="submit" onPress={handleSubmit(onSubmit)} />
     </View>
-  );
+  )
 }
+
+
+// export default function App() {
+
+//   const [open, setOpen] = useState(false);
+//   const [currentValue, setCurrentValue] = useState(null);
+//   const [items, setItems] = useState([{}]);
+//   const { register, setValue, handleSubmit } = useForm<FormInput>();
+
+//   const RegisterForm = () => {
+//     return (
+//       <View style={styles.container}>
+//         <Text>Email</Text>
+//         <TextInput {...register('email')} />
+//         <Text>Password</Text>
+//         <TextInput {...register('password', { required: true})} />
+//         <DropDownPicker 
+//           items={items}
+//           open={open}
+//           value={currentValue}
+//           setOpen={setOpen}
+//           setValue={setCurrentValue}
+//           setItems={setItems}
+//         />
+//       </View>
+//     )
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <RegisterForm />
+//     </View>
+//   );
+// }
 
 const styles = StyleSheet.create({
   container: {
@@ -50,4 +98,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textInput: {
+    backgroundColor: '#d3d3d3',
+    margin: 10,
+    paddingLeft: 5,
+  }
 });
