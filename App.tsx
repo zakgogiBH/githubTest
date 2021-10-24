7c5599d50f30d4f3f61f7b8486ee6ebde7007b58
zakgogi <zakgogi1@gmail.com>
2021-10-24 19:36:08 +0100

    Initial form setup

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useForm } from 'react-hook-form';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+
+interface FormInput {
+  email: string, 
+  password: string;
+}
 
 export default function App() {
+
+  const [open, setOpen] = useState(false);
+  const [currentValue, setCurrentValue] = useState(null);
+  const [items, setItems] = useState([{}]);
+  const { register, setValue, handleSubmit } = useForm<FormInput>();
+
+  const RegisterForm = () => {
+    return (
+      <View style={styles.container}>
+        <Text>Email</Text>
+        <TextInput {...register('email')} />
+        <Text>Password</Text>
+        <TextInput {...register('password', { required: true})} />
+        <DropDownPicker 
+          items={items}
+          open={open}
+          value={currentValue}
+          setOpen={setOpen}
+          setValue={setCurrentValue}
+          setItems={setItems}
+        />
+      </View>
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <RegisterForm />
     </View>
   );
 }
